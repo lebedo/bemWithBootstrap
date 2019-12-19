f899141996d2efc728d3a1f54d91131f770b666d
olga <bendugo5@gmail.com>
2019-12-19 22:36:26 +0200

    адаптивній сайт

--- js/index.js
+++ js/index.js
@@ -50,6 +50,7 @@ $(document).ready(function() {
             $(' .filter > div').show(300);
         }
     });
+    /*Slider*/
     $('.sl').slick({
         infinite: true,
         slidesToShow: 3,
@@ -60,10 +61,51 @@ $(document).ready(function() {
         responsive: [{
             breakpoint: 1024,
             settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2,
+                slidesToShow: 1,
+                slidesToScroll: 1
             }
         }]
     });
+    /*menu */
+
+    $('ul.menu a[href^="#"]').click(function() {
+        var target = $(this).attr('href');
+        $('html, body').animation({
+            scrollTop: $(target).offset().top
+        }, 500);
+        $('ul.menu a[href^="#"]').css({ 'color': '#212121' });
+        $(this).css({ 'color': '#00897b' });
+        return false;
+    });
+
+    /*кнопка наверх не работает! */
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() != 0) {
+            $('#toTop').fadeIn();
+        } else {
+            $('#toTop').fadeOut();
+        }
+    });
+    $('#toTop').click(function() {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 800);
+    });
+    /*выпадающее меню */
+
+    $('.menu_icon').click(function() {
+        $('nav').slideToggle(500);
+        $('ul.menu').css({
+            'display': 'flex',
+            'flex-direction': 'column',
+            'align-items': 'center',
+        })
+        if ($('.menu_icon').html() == '<i class="fas fa-bars"></i>') {
+            $(this).html('<i class="fas fa-times"></i>')
+        } else {
+            $(this).html('<i class="fas fa-bars"></i>')
+        }
+    })
 
 })
